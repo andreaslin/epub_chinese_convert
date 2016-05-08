57bbc02f0a9df81a2771ccfd4df8a0495e9145e6
Andreas <andreas.yuyi@gmail.com>
2016-05-08 10:01:08 -0700

    Update README. Starting to integrate pyopencc to script.

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
-# epub_convert
-A script that uses opencc command line tool to convert epub files.
+# epub_chinese_convert epub中文簡繁體轉換
+Using [opencc](https://github.com/BYVoid/OpenCC) and [pyopencc](https://github.com/cute/pyopencc) to convert epub file between Chinese characters.
 
-# Disclaimer
-Please read the following before use.
+用[opencc](https://github.com/BYVoid/OpenCC)和[pyopencc](https://github.com/cute/pyopencc)來做epub格式的簡繁體中文轉換
 
-This script is not fully tested, use it at your own risk.
-Only tested under MAC OS X.
+## Installation 安裝
+### opencc
+Please refer to opencc [Installation-安裝](https://github.com/BYVoid/OpenCC#installation-安裝) page
 
-# Notes:
-  1. This script is created since http://epubconv.mobi/ can only take file under 10MB.
+## Usage 用法
+### epub_chinese_convert
+```
+epub_convert_chinese.py [-h] -i INPUT_FILE [-o OUTPUT_FILE] -c CONFIG_OPTION [-v VERBOSE]
+```
+
+## Example 範例
+```
+  python epub_chinese_convert.py -i my_book.epub -c s2tw
+```
+
+## Configurations 設定文件
+Please refer to opencc [Configurations-配置文件](https://github.com/BYVoid/OpenCC#configurations-配置文件) page
+
+## License 使用許可
+MIT License
+
+## Notes
+  1. This script is created since http://epubconv.mobi/ can only take file under 10MB (Update - May 7, 2016: The site is down)
   2. No encoding check for the file; encoding is assume to be UTF-8
-  3. Large files can take up to 2 minutes to complete (tested 12MB epub file)
+  3. Large files can take up to 2 minutes to complete (E.g. 10MB+ files)
+
+## TODO
+  1. Speed up converting time
+  2. Deal with encoding
+  3. Setup a website for online convert
--- src/epub_convert_chinese.py
+++ src/epub_convert_chinese.py
@@ -6,34 +6,43 @@ from subprocess import check_output, Popen, PIPE
 from shutil import copy
 from os.path import dirname, realpath, basename, splitext
 from multiprocessing import Pool
-import os
-import argparse
-import zipfile
-import zlib
+import os, argparse
+import zipfile, zlib
 
-class epub_convert:
+class EpubChineseConvert:
+    ALL_CONFIGS = ["s2t", "t2s", "s2tw", "tw2s", "s2hk", "hk2s", "s2twp",
+                        "tw2sp", "t2tw", "t2hk"]
+    CONVERT_EXTENSIONS = ("html", "htm", "ncx", "opf", "xhtml")
+    # The limit size prevent the stdout from overflow, this is system dependence.
+    # The size I use here is for Mac OS X
+    STDOUT_PIPE_BUFFER_LIMIT = 65536
+
     def __init__(self, options):
-        self.OPENCC_BIN_PATH = check_output(["which", "opencc"]).strip()    
+        # Old way of using opencc from command line
+        self.OPENCC_BIN_PATH = check_output(["which", "opencc"]).strip()
         self.OPENCC_CONFIG_PATH = check_output(["find", "/usr/local",
                                                 "-name", options.config_option+".json"]).strip()
-        self.ALL_CONFIGS = ["s2t", "t2s", "s2tw", "tw2s", "s2hk", "hk2s", "s2twp",
-                            "tw2sp", "t2tw", "t2hk"]
-        self.CONVERT_EXTENSIONS = ("html", "htm", "ncx", "opf", "xhtml")
+
         self.ABSOLUTE_INPUT_PATH = dirname(realpath(options.input_file))
         self.INPUT_FILE_NAME = splitext(basename(realpath(options.input_file)))[0]
-        
+
+        self.VERBOSE = False
+        self.COMPRESSION_MODE = zipfile.ZIP_DEFLATED
+
+        if not options.config_option in EpubChineseConvert.ALL_CONFIGS:
+            print("Configuration unavailable. Please use the following: ")
+            print EpubChineseConvert.ALL_CONFIGS
+            exit(1)
+
         if not options.output_file:
             self.ABSOLUTE_OUTPUT_PATH = self.ABSOLUTE_INPUT_PATH
-            self.OUTPUT_FILE_NAME = self.INPUT_FILE_NAME + "_output"
+            self.OUTPUT_FILE_NAME = self.INPUT_FILE_NAME + "_" + options.config_option
         else:
             self.ABSOLUTE_OUTPUT_PATH = dirname(realpath(options.output_file))
             self.OUTPUT_FILE_NAME = basename(self.ABSOLUTE_OUTPUT_PATH)
 
-        if not options.config_option in self.ALL_CONFIGS:
-            print("Configuration unavailable. Please use the following: ")
-            print self.ALL_CONFIGS
-            exit(1)
-            
+        if options.verbose: self.VERBOSE = True
+
     def checkInstall(self):
         if not self.OPENCC_BIN_PATH:
             print("Cannot find opencc command, please check if opencc is installed or linked")
@@ -41,7 +50,7 @@ class epub_convert:
 
         if not self.OPENCC_CONFIG_PATH:
             print("Cannot find configuration file")
-            exit(1)        
+            exit(1)
 
     def checkOutputPathReadWritePermissions(self):
         try:
@@ -49,7 +58,7 @@ class epub_convert:
         except OSError:
             print ("No output directory:" + self.ABSOLUTE_OUTPUT_PATH + " permission")
             exit(1)
-            
+
     def convertContent(self, input_bytes):
         try:
             convert_result = Popen(
@@ -61,22 +70,25 @@ class epub_convert:
             print e
             print "Error while using opencc."
             raise e
-                      
+
     def convert(self):
         self.checkInstall()
         self.checkOutputPathReadWritePermissions()
 
+        # Extract file locations
         epub_files = zipfile.ZipFile(self.ABSOLUTE_INPUT_PATH + "/" + self.INPUT_FILE_NAME + ".epub", mode='r')
         epub_files_output = zipfile.ZipFile(self.ABSOLUTE_OUTPUT_PATH + "/" + self.OUTPUT_FILE_NAME + ".epub",
                                             mode='w')
-        compression_mode = zipfile.ZIP_DEFLATED
         num_file_converted = 0
+        file_list = epub_files.infolist()
+        total_num_files = len(file_list)
         try:
-            for zipinfo_file in epub_files.infolist():
+            for i, zipinfo_file in enumerate(file_list):
                 content = ""
-                if zipinfo_file.filename.endswith(self.CONVERT_EXTENSIONS):
-                    print "Converting " + zipinfo_file.filename + "..."
-                    if zipinfo_file.file_size > 300000:
+                if zipinfo_file.filename.endswith(EpubChineseConvert.CONVERT_EXTENSIONS):
+                    if self.VERBOSE:
+                        print "Converting {}...".format(zipinfo_file.filename)
+                    if zipinfo_file.file_size >= EpubChineseConvert.STDOUT_PIPE_BUFFER_LIMIT:
                         with epub_files.open(zipinfo_file) as f:
                             for line in f:
                                 content += self.convertContent(line.replace('zh-CN', 'zh-TW'))
@@ -86,8 +98,9 @@ class epub_convert:
                     num_file_converted += 1
                 else:
                     content = epub_files.read(zipinfo_file)
+                print "Processed file {}/{} \r".format(i, total_num_files),
                 epub_files_output.writestr(zipinfo_file, content,
-                                           compress_type = compression_mode)
+                                           compress_type = self.COMPRESSION_MODE)
         except Exception, e:
             print e
             print "Error while extracting/converting..."
@@ -101,27 +114,29 @@ class epub_convert:
             epub_files_output.close()
 
         return num_file_converted
-            
-        
+
+
 def parseOptions():
     parser = argparse.ArgumentParser(description=
-                                     "Convert epub file using opencc in python")
+                                     "Convert between Traditional/Simplified Chinese characters in epub file")
     parser.add_argument('-i', '--input_file', required=True,
                         help="The name of the input file.")
     parser.add_argument('-o', '--output_file',
                         help="The name of the output file (if specified).")
     parser.add_argument('-c', '--config_option', required=True,
-                        help="The configuration file used, refer to opencc json config files.")
+                        help="The configuration file used, refer to opencc json config files.\n" + ','.join(EpubChineseConvert.ALL_CONFIGS))
+    parser.add_argument('-v', '--verbose',
+                        help="Verbose output")
     return parser.parse_args()
-            
+
 def main():
-    convertor = epub_convert(parseOptions())
+    convertor = EpubChineseConvert(parseOptions())
     convert_num = convertor.convert();
     if convert_num:
         print "Successfully converted " + str(convert_num) + " items."
     else:
         print "Error"
-    
+
     return 0
 
 if __name__ == "__main__":
